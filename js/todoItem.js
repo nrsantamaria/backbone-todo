77d60a203701478d5e4f7f292498f5ac3135a999
Nicole Santamaria <nicolersantamaria@gmail.com>
2018-02-19 19:24:20 -0800

    add completed checkboxes and styling.

--- js/todoItem.js
+++ js/todoItem.js
@@ -1,6 +1,15 @@
 var ToDoItem = Backbone.Model.extend({
+  defaults: {
+    isCompleted: false
+  },
+
   validate: function(attrs){
     if(!attrs.description)
       return "Description is required.";
+  },
+
+  toggle: function(){
+      this.set("isCompleted", !this.get("isCompleted"));
+      //whatever value the isCompleted property has we get it we invert it and then store it back in itself
   }
 });
--- js/todoItemView.js
+++ js/todoItemView.js
@@ -4,10 +4,23 @@ var ToDoItemView = Backbone.View.extend({
   initialize: function(options){
     if(!(options && options.model))
       throw new Error("model is not specified");
+    this.model.on("change", this.render, this);
+  },
+
+  events: {
+    "click #toggle": "onClickToggle"
+  },
+
+  onClickToggle: function(){
+    this.model.toggle();
   },
 
   render: function(){
-    this.$el.html(this.model.escape("description"));
+    this.$el.toggleClass("completed", this.model.get("isCompleted"));
+
+    var checked = this.model.get("isCompleted") ? "checked": "";//first get the value of the isCompleted property, and if its checked, render a checked attribute in the input below
+
+    this.$el.html("<input id='toggle' type='checkbox'"   + checked + "></input>" + this.model.escape("description"));
     //escape is like the get method but it also html encodes the return value so you cant hack the text box with javascript code
     return this;
   }
