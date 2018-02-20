eae88ae6b206be420d10977885a94875bae5698b
Nicole Santamaria <nicolersantamaria@gmail.com>
2018-02-19 17:00:03 -0800

    add text box and button to view.

--- js/todoItemView.js
+++ js/todoItemView.js
@@ -1,14 +1,14 @@
 var ToDoItemView = Backbone.View.extend({
   tagName: "li",
-  
+
   initialize: function(options){
     if(!(options && options.model))
       throw new Error("model is not specified");
   },
 
   render: function(){
-    this.$el.html(this.model.get("description"));
-
+    this.$el.html(this.model.escape("description"));
+    //escape is like the get method but it also html encodes the return value so you cant hack the text box with javascript code
     return this;
   }
 })
--- js/todoItemsView.js
+++ js/todoItemsView.js
@@ -6,10 +6,38 @@ var ToDoItemsView = Backbone.View.extend({
   initialize: function(options){
     if(!(options && options.model))
       throw new Error("model is not specified");
+
+    this.model.on("add", this.onAddTodoItem, this);
+  },
+
+  onAddTodoItem: function(todoItem){
+    var view = new ToDoItemView({model: todoItem});
+    this.$el.append(view.render().$el);
+  },
+
+  events: {
+    "click #add": "onClickAdd",
+    "keypress #newToDoItem": "onKeyPress"
+  },
+
+  onKeyPress: function(e){
+    if(e.keyCode == 13)
+      this.onClickAdd();
+  },
+
+  onClickAdd: function(){
+    var $textbox = this.$("#newToDoItem");
+    if($textbox.val()){
+      var todoitem = new ToDoItem({ description: $textbox.val() });
+      this.model.add(todoitem);
+      $textbox.val("");
+    }
   },
 
   render: function(){
     var self = this;
+    this.$el.append("<input type='text' id='newToDoItem' autofocus></input>");
+    this.$el.append("<button id='add'>Add</button>");
 
     this.model.each(function(toDoItem){
       var view = new ToDoItemView({ model: toDoItem });
